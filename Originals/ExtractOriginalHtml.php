1a6605b5badc270e947c806e19da1591e1e3de31
Richard Welsh <richard@welshdesign.co.uk>
2018-02-10 09:52:24 +0000

    Added iznik-server as submodule; started on illustrations artwork

---
+++ Originals/ExtractOriginalHtml.php
@@ -0,0 +1,12 @@
+<?php
+
+define('IZNIK_BASE', '../iznik-server/');
+
+require_once(IZNIK_BASE . 'mailtemplates/welcome/withpassword.php');
+file_put_contents('./welcome_withpassword.html', welcome_password('www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'rdwelsh@gmail.com', 'pa55w0rd'));
+
+require_once(IZNIK_BASE . 'mailtemplates/welcome/forgotpassword.php');
+file_put_contents('./welcome_forgotpassword.html', forgot_password('www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'rdwelsh@gmail.com', 'https://someLink.com/12345'));
+
+require_once(IZNIK_BASE . 'mailtemplates/welcome/group.php');
+file_put_contents('./welcome_group.html', welcome_group('www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'rdwelsh@gmail.com', 'Group-Name', 'This is a welcome-to-group message.'));
