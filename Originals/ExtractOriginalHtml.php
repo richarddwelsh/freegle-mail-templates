6cfd4b08a8fa9602da858c581b3298e6c69b969a
Richard Welsh <richard@welshdesign.co.uk>
2018-02-10 10:49:57 +0000

    Moved iznik-server up a level out of repo

--- Originals/ExtractOriginalHtml.php
+++ Originals/ExtractOriginalHtml.php
@@ -1,6 +1,6 @@
 <?php
 
-define('IZNIK_BASE', '../iznik-server/');
+define('IZNIK_BASE', '../../iznik-server/');
 
 require_once(IZNIK_BASE . 'mailtemplates/welcome/withpassword.php');
 file_put_contents('./welcome_withpassword.html', welcome_password('www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'rdwelsh@gmail.com', 'pa55w0rd'));
---
+++ Originals/README.md
@@ -0,0 +1,7 @@
+## Originals folder
+
+* `iznik-server` repo must be cloned alongside this one
+* edit `ExtractOriginalHtml.php` to extract required HTML from `iznik-server`, then run it with 
+```
+php ExtractOriginalHtml.php
+```
