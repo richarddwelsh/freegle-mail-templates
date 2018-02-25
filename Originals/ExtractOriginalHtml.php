807d475420f3dbc58797d533a6561cd608e6dd6a
Unknown <richard@welshdesign.co.uk>
2018-02-25 18:24:05 +0000

    Working on Artwork; some tweaks to MJML

--- Originals/ExtractOriginalHtml.php
+++ Originals/ExtractOriginalHtml.php
@@ -10,3 +10,14 @@ file_put_contents('./welcome_forgotpassword.html', forgot_password('www.ilovefre
 
 require_once(IZNIK_BASE . 'mailtemplates/welcome/group.php');
 file_put_contents('./welcome_group.html', welcome_group('www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'rdwelsh@gmail.com', 'Group-Name', 'This is a welcome-to-group message.'));
+
+
+
+require_once(IZNIK_BASE . 'mailtemplates/digest/message.php');
+file_put_contents('./digest_message.html', digest_message('<b>Message here</b>', 'www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'Group-Name', 'Subject'));
+
+require_once(IZNIK_BASE . 'mailtemplates/digest/single.php');
+file_put_contents('./digest_single.html', digest_single('<b>Message here</b>', 'www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'Group-Name', 'Subject'));
+
+require_once(IZNIK_BASE . 'mailtemplates/digest/multiple.php');
+file_put_contents('./digest_multiple.html', digest_multiple('<b>Message here</b>', 'www.ilovefreegle.org', 'https://www.ilovefreegle.org/images/user_logo.png', 'Group-Name', 'Subject'));
